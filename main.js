6534d61b7eee3bdc5bc7d21d43d3a2c85e5b8ff4
carflor <cfdeclet@gmail.com>
2020-02-19 16:28:48 -0700

    Invoke populateSelected

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ window.onload = invokeDeck();
 cardContainer.addEventListener('click', clickHandler)
 
 // Functions
+
+function clickHandler(event) {
+  if(event.target.closest('.card')) {
+    deck.populateSelected(event);
+  }
+}
+
 function invokeDeck() {
   deck.populateDeck();
   displayCards();
@@ -17,19 +24,15 @@ function displayCards() {
   var cardCounter = 0;
   for (var i = 0; i < deck.cards.length; i++) {
     cardCounter++;
-    deck.cards[i].matchInfo = cardCounter;
+    deck.cards[i].matchInfo = `${cardCounter}`;
     cardContainer.insertAdjacentHTML('beforeend', `
     <div id="${cardCounter}" class="card card-${cardCounter}">
       <p>B</p>
     </div>`)
+    
   }
 }
 
-function clickHandler() {
-  if(event.target.closest('.card')) {
-    deck.populateSelected();
-  }
-}
 
 
 
