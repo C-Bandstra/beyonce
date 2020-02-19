e60516b5940bdfe295d26c368c73a8d697e89e53
Ed Ignot <edignot@gmail.com>
2020-02-19 15:27:06 -0700

    removed card template from html and added insertadjacentHTML

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ var deck = new Deck();
 
 window.onload = invokeDeck();
 
-
-// function displayCards() {
-//   for ( var i = 0 ; i < this. ; i++ ) {
-//
-//   }
-// }
-
 function invokeDeck() {
   deck.populateDeck();
+  displayCards();
+}
+
+function displayCards() {
+  debugger
+  for (var i = 0; i < deck.cards.length; i++) {
+    cardContainer.insertAdjacentHTML('beforeend', `
+    <div class="card">
+      <p>B</p>
+    </div>`)
+  }
 }
