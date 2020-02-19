72da86344231dc404fe602ff1a951e3ae033b3de
carflor <cfdeclet@gmail.com>
2020-02-19 15:54:22 -0700

    Add clickHandler fn and cardContainer eventListener

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+// Variables 
 var cardContainer = document.querySelector('.card-container');
 var deck = new Deck();
 
 window.onload = invokeDeck();
 
+// Event Listeners  
+cardContainer.addEventListener('click', clickHandler)
+
+// Functions
 function invokeDeck() {
   deck.populateDeck();
   displayCards();
@@ -12,10 +17,19 @@ function displayCards() {
   var cardCounter = 0;
   for (var i = 0; i < deck.cards.length; i++) {
     cardCounter++;
+    deck.cards[i].matchInfo = cardCounter;
     cardContainer.insertAdjacentHTML('beforeend', `
     <div id="${cardCounter}" class="card card-${cardCounter}">
       <p>B</p>
     </div>`)
   }
-  console.log(cardCounter);
 }
+
+function clickHandler() {
+  if(event.target.closest('.card')) {
+    deck.populateSelected();
+  }
+}
+
+
+
