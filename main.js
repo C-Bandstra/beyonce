e29348e5ac0bd792f3014468f0c54c98dec0dcd9
carflor <cfdeclet@gmail.com>
2020-02-20 08:48:45 -0700

    Sketch checkSelected and displayCards for loops

--- main.js
+++ main.js
@@ -12,27 +12,35 @@ cardContainer.addEventListener('click', clickHandler)
 function clickHandler(event) {
   if(event.target.closest('.card')) {
     deck.populateSelected(event);
+    displayCards(event);
   }
 }
 
 function invokeDeck() {
   deck.populateDeck();
-  displayCards();
+  displayCards(event);
 }
 
-function displayCards() {
+function displayCards(event) {
   var cardCounter = 0;
   for (var i = 0; i < deck.cards.length; i++) {
     cardCounter++;
     deck.cards[i].matchInfo = `${cardCounter}`;
+    // checkSelected(cardCounter, event);
     cardContainer.insertAdjacentHTML('beforeend', `
     <div id="${cardCounter}" class="card card-${cardCounter}">
       <p>B</p>
-    </div>`)
-    
+    </div>`) 
+    checkSelected(cardCounter, event)
   }
 }
 
-
-
-
+function checkSelected(cardCounter, event) {
+  debugger
+  for(var i = 0; i < deck.cards.length; i++) {
+    if(deck.cards[i].selected === true) {
+      event.target.classList.add(`img-${cardCounter}`)
+      break
+    }
+  }
+}
