438a4166f9d9f522b72ec71ede12d8f16dffa005
Charlie Bandstra <bandstracharlie@gmail.com>
2020-02-21 16:43:09 -0700

    Refactored loops and split up functions, added data model match functionality

--- deck.js
+++ deck.js
@@ -9,45 +9,47 @@ class Deck {
     //when the user starts a new game, the cards should be shuffled
   }
 
-  checkSelected(card) {
-    //when a card is clicked, it should be moved to the selectedCards array
-    //if a card is clicked a second time, there should still only be one of that
-    //card in the selectedCards array
-  }
-
   moveToMatched(card) {
-    //if 2 cards are matched, move that pair to the matchedCards array
-  }
+    var selectedArr = this.selectedCards
+      if(selectedArr[0].matchInfo === selectedArr[1].matchInfo) {
+        selectedArr.forEach(card => {
+          card.matched = true
+        })
+        this.matchedCards.push(selectedArr);
+      }
+    }
+
   populateDeck() {
     var firstCardsId = 0;
     var lastCardsId = 5;
     for (var i = 1; i < 6; i++) {
       firstCardsId++;
-      var card = new Card(`card-${i}`, firstCardsId);
-      this.cards.push(card);
-    }
-    for (var i = 1; i < 6; i++) {
       lastCardsId++;
-      var card = new Card(`card-${i}`, lastCardsId);
-      this.cards.push(card);
+      var card1 = new Card(`card-${i}`, firstCardsId);
+      var card2 = new Card(`card-${i}`, lastCardsId);
+      this.cards.push(card1);
+      this.cards.push(card2);
     }
     console.log(this.cards);
   }
 
-  populateSelected(event) {
+  checkSelected(event) {
     var selectedArr = this.selectedCards;
-    for (var i = 0; i < this.cards.length; i++) {
-      var cardIndex = this.cards[i];
-      if (cardIndex.id == event.target.id && selectedArr.length <= 2) {
-        if (!cardIndex.selected && selectedArr.length < 2) {
-          selectedArr.push(cardIndex);
-          cardIndex.selected = true;
-          console.log(selectedArr);
-          break;
-        } else {
-          this.removeSelected(selectedArr);
+    this.cards.forEach((card, i) => {
+        var cardIndex = this.cards[i];
+        if (cardIndex.id == event.target.id && selectedArr.length <= 2) {
+          this.populateSelected(selectedArr, cardIndex)
         }
-      }
+    })
+  }
+
+  populateSelected(selectedArr, cardIndex) {
+    if (!cardIndex.selected && selectedArr.length < 2) {
+      selectedArr.push(cardIndex);
+      cardIndex.selected = true;
+      console.log(selectedArr);
+    } else {
+      this.removeSelected(selectedArr);
     }
   }
 
