3b951b8b49859b989d3f015f2da6d10baf6c6491
carflor <cfdeclet@gmail.com>
2020-02-21 18:07:24 -0700

    Add sketch of matchedCards function

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ cardContainer.addEventListener('click', clickHandler);
 function clickHandler(event) {
   if (event.target.closest('.card')) {
     deck.checkSelected(event);
-    checkSelected(event);
+    displayImg(event);
   }
   if(deck.selectedCards.length === 2) {
     deck.moveToMatched();
@@ -35,7 +35,7 @@ function displayCards(event) {
   }
 }
 
-function checkSelected(event) {
+function displayImg(event) {
   for (var i = 0; i < deck.cards.length; i++) {
     var cardId = deck.cards[i].id;
     var imgId = cardId > 5 ? cardId - 5 : cardId;
@@ -51,3 +51,23 @@ function checkSelected(event) {
     }
   }
 }
+
+function removeMatchedDom() {
+  debugger
+  //check if cards in selected cards Match are True
+  deck.matchedCards.forEach((match, i) => {
+    match.forEach(card => {
+      if(event.target.classList.contains('matched')) {
+        event.target.remove()
+      }
+    })
+  }) 
+  
+  //check if matchInfo matches cardClass 
+  //then remove both cards from selected array
+  //also removes Divs from card-container 
+} 
+
+function addMatchedClass() {
+  event.target.classList.add('matched');
+} 
